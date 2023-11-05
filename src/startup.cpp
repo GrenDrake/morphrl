4d3f80adadc68edc98f964d2b1478a791a41abef
Gren Drake <gren@grenslair.com>
2023-11-05 06:16:51 -0800

    Fixes mouse support on main menu

--- src/startup.cpp
+++ src/startup.cpp
@@ -129,6 +129,10 @@ World* createGame() {
     return world;
 }
 
+struct UIRect {
+    int x, y, w, h, ident;
+};
+
 int main() {
     srand(time(nullptr));
     std::cerr << "LOADING DATA\n";
@@ -140,6 +144,7 @@ int main() {
     terminal_set("font: resources/DejaVuSansMono.ttf, size=24;");
     terminal_set("italic font: resources/DejaVuSansMono-Oblique.ttf, size=24;");
 
+    std::vector<UIRect> mouseRegions;
     World *world = nullptr;
     std::cerr << "ENTERING main menu\n";
     const std::string versionString = "Development Release 1";
@@ -151,17 +156,23 @@ int main() {
     color_t fgColorDark = color_from_argb(255, 98, 98, 98);
     color_t bgColor = color_from_argb(255, 0, 0, 0);
     while (!done) {
+        mouseRegions.clear();
         terminal_color(fgColor);
         terminal_bkcolor(bgColor);
         terminal_clear();
         terminal_print(25, 11, "[font=italic]Delving the Mutagenic Dungeon");
         terminal_print(versionX, 24, ("[font=italic]" + versionString).c_str());
         terminal_print(8, 16, "Start new game");
+        mouseRegions.push_back(UIRect{8, 16, 20, 1, 0});
         terminal_print(8, 17, "Resume current game");
+        mouseRegions.push_back(UIRect{8, 17, 20, 1, 1});
         terminal_print(8, 18, "Story");
+        mouseRegions.push_back(UIRect{8, 18, 20, 1, 2});
         terminal_print(8, 19, "Credits");
+        mouseRegions.push_back(UIRect{8, 19, 20, 1, 3});
         terminal_print(8, 20, "Quit");
-        terminal_put(6, 16+selection, '*');
+        mouseRegions.push_back(UIRect{8, 20, 20, 1, 4});
+        terminal_print(5, 16+selection, "->");
         terminal_color(fgColorDark);
         terminal_print(31, 12, "Pre-Alpha Release");
         drawImage(0, 0, logo);
@@ -175,6 +186,18 @@ int main() {
         if ((key == TK_UP || key == TK_KP_8) && selection > 0) --selection;
         if (key == TK_END) selection = 4;
         if ((key == TK_DOWN || key == TK_KP_2) && selection < 4) ++selection;
+
+        if (key == TK_MOUSE_LEFT) {
+            int mx = terminal_state(TK_MOUSE_X);
+            int my = terminal_state(TK_MOUSE_Y);
+            for (const UIRect &rect : mouseRegions) {
+                if (mx < rect.x || mx >= rect.x + rect.w) continue;
+                if (my < rect.y || my >= rect.y + rect.h) continue;
+                selection = rect.ident;
+                key = TK_SPACE;
+                break;
+            }
+        }
         if (key == TK_SPACE || key == TK_ENTER || key == TK_KP_ENTER) {
             switch(selection) {
                 case 0: {
@@ -254,8 +277,8 @@ void showDocument(DocStory &story) {
         terminal_refresh();
 
         int key = terminal_read();
-        if (key == TK_LEFT && currentPage > 0) --currentPage;
-        if (key == TK_SPACE || key == TK_ENTER) {
+        if ((key == TK_LEFT || key == TK_MOUSE_RIGHT) && currentPage > 0) --currentPage;
+        if (key == TK_SPACE || key == TK_ENTER || key == TK_MOUSE_LEFT) {
             if (currentPage < story.pages.size() - 1) ++currentPage;
             else return;
         }
